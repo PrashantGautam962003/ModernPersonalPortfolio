702e929b29e21894096d3203be1cc083ac352076
PrashantGautam962003 <prashantgautam962003@gmail.com>
2025-10-31 11:53:38 +0530

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -37,7 +37,7 @@ export const AboutMe = () => {
               </a>
 
               <a
-                href="https://drive.google.com/drive/u/0/folders/1ZeTmttaV4MQ59ImHb7hHCSTxE4vYN0BT"
+                href="https://drive.google.com/file/d/10mqBsakvFIrQPMg75nqejjmhOeIcCOIE/view?usp=drivesdk"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
